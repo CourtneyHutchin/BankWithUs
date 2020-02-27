ab692caa6fe4b596c645be1bfc44eeff6f069e99
KarloParagas <karloparagas@gmail.com>
2020-02-27 07:44:36 -0800

    Removed unused FormLoad

--- BankWithUs/RegisterForm.Designer.cs
+++ BankWithUs/RegisterForm.Designer.cs
@@ -227,7 +227,6 @@
             this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "RegisterForm";
             this.Text = "Register";
-            this.Load += new System.EventHandler(this.RegisterForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
