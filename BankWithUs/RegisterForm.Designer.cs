a3436974c9fa4d78c1c02bf9def58766692043f6
KarloParagas <karloparagas@gmail.com>
2020-02-26 21:57:38 -0800

    Fixed WinForm size

--- BankWithUs/RegisterForm.Designer.cs
+++ BankWithUs/RegisterForm.Designer.cs
@@ -186,7 +186,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 286);
+            this.ClientSize = new System.Drawing.Size(470, 297);
             this.Controls.Add(this.DebitCardNumTxt);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.LastNameTxt);
