62fdd6eb5565c6bc06161129282431dfff412c72
KarloParagas <karloparagas@gmail.com>
2020-02-27 07:20:34 -0800

    Add missing input field for password

--- BankWithUs/RegisterForm.Designer.cs
+++ BankWithUs/RegisterForm.Designer.cs
@@ -28,165 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.RegisterBtn = new System.Windows.Forms.Button();
-            this.FirstNameTxt = new System.Windows.Forms.TextBox();
-            this.FirstDepositTxt = new System.Windows.Forms.TextBox();
-            this.AddressTxt = new System.Windows.Forms.TextBox();
-            this.PhoneNumberTxt = new System.Windows.Forms.TextBox();
-            this.EmailTxt = new System.Windows.Forms.TextBox();
-            this.LastNameTxt = new System.Windows.Forms.TextBox();
+            this.PasswordTxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.DebitCardNumTxt = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.LastNameTxt = new System.Windows.Forms.TextBox();
+            this.EmailTxt = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTxt = new System.Windows.Forms.TextBox();
+            this.AddressTxt = new System.Windows.Forms.TextBox();
+            this.FirstDepositTxt = new System.Windows.Forms.TextBox();
+            this.FirstNameTxt = new System.Windows.Forms.TextBox();
+            this.RegisterBtn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // PasswordTxt
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 20);
-            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "First Name:";
+            this.PasswordTxt.Location = new System.Drawing.Point(112, 132);
+            this.PasswordTxt.Margin = new System.Windows.Forms.Padding(1);
+            this.PasswordTxt.Name = "PasswordTxt";
+            this.PasswordTxt.Size = new System.Drawing.Size(338, 20);
+            this.PasswordTxt.TabIndex = 18;
             // 
-            // label2
+            // label8
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 54);
-            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Last Name:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 86);
-            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "E-Mail:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 163);
-            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Phone Number:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 197);
-            this.label5.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Home Address:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 231);
-            this.label6.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(68, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "First Deposit:";
-            // 
-            // RegisterBtn
-            // 
-            this.RegisterBtn.Location = new System.Drawing.Point(251, 263);
-            this.RegisterBtn.Margin = new System.Windows.Forms.Padding(1);
-            this.RegisterBtn.Name = "RegisterBtn";
-            this.RegisterBtn.Size = new System.Drawing.Size(76, 22);
-            this.RegisterBtn.TabIndex = 6;
-            this.RegisterBtn.Text = "Register";
-            this.RegisterBtn.UseVisualStyleBackColor = true;
-            // 
-            // FirstNameTxt
-            // 
-            this.FirstNameTxt.Location = new System.Drawing.Point(120, 17);
-            this.FirstNameTxt.Margin = new System.Windows.Forms.Padding(1);
-            this.FirstNameTxt.Name = "FirstNameTxt";
-            this.FirstNameTxt.Size = new System.Drawing.Size(338, 20);
-            this.FirstNameTxt.TabIndex = 0;
-            // 
-            // FirstDepositTxt
-            // 
-            this.FirstDepositTxt.Location = new System.Drawing.Point(120, 228);
-            this.FirstDepositTxt.Margin = new System.Windows.Forms.Padding(1);
-            this.FirstDepositTxt.Name = "FirstDepositTxt";
-            this.FirstDepositTxt.Size = new System.Drawing.Size(338, 20);
-            this.FirstDepositTxt.TabIndex = 5;
-            // 
-            // AddressTxt
-            // 
-            this.AddressTxt.Location = new System.Drawing.Point(120, 195);
-            this.AddressTxt.Margin = new System.Windows.Forms.Padding(1);
-            this.AddressTxt.Multiline = true;
-            this.AddressTxt.Name = "AddressTxt";
-            this.AddressTxt.Size = new System.Drawing.Size(338, 18);
-            this.AddressTxt.TabIndex = 4;
-            // 
-            // PhoneNumberTxt
-            // 
-            this.PhoneNumberTxt.Location = new System.Drawing.Point(120, 160);
-            this.PhoneNumberTxt.Margin = new System.Windows.Forms.Padding(1);
-            this.PhoneNumberTxt.Name = "PhoneNumberTxt";
-            this.PhoneNumberTxt.Size = new System.Drawing.Size(338, 20);
-            this.PhoneNumberTxt.TabIndex = 3;
-            // 
-            // EmailTxt
-            // 
-            this.EmailTxt.Location = new System.Drawing.Point(120, 84);
-            this.EmailTxt.Margin = new System.Windows.Forms.Padding(1);
-            this.EmailTxt.Name = "EmailTxt";
-            this.EmailTxt.Size = new System.Drawing.Size(338, 20);
-            this.EmailTxt.TabIndex = 2;
-            // 
-            // LastNameTxt
-            // 
-            this.LastNameTxt.Location = new System.Drawing.Point(120, 52);
-            this.LastNameTxt.Margin = new System.Windows.Forms.Padding(1);
-            this.LastNameTxt.Name = "LastNameTxt";
-            this.LastNameTxt.Size = new System.Drawing.Size(338, 20);
-            this.LastNameTxt.TabIndex = 1;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 134);
+            this.label8.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "Password:";
             // 
             // DebitCardNumTxt
             // 
-            this.DebitCardNumTxt.Location = new System.Drawing.Point(120, 121);
+            this.DebitCardNumTxt.Location = new System.Drawing.Point(114, 171);
             this.DebitCardNumTxt.Margin = new System.Windows.Forms.Padding(1);
             this.DebitCardNumTxt.Name = "DebitCardNumTxt";
             this.DebitCardNumTxt.Size = new System.Drawing.Size(338, 20);
-            this.DebitCardNumTxt.TabIndex = 7;
+            this.DebitCardNumTxt.TabIndex = 20;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(16, 123);
+            this.label7.Location = new System.Drawing.Point(8, 174);
             this.label7.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(100, 13);
-            this.label7.TabIndex = 8;
+            this.label7.TabIndex = 25;
             this.label7.Text = "Debit Card Number:";
+            // 
+            // LastNameTxt
+            // 
+            this.LastNameTxt.Location = new System.Drawing.Point(112, 55);
+            this.LastNameTxt.Margin = new System.Windows.Forms.Padding(1);
+            this.LastNameTxt.Name = "LastNameTxt";
+            this.LastNameTxt.Size = new System.Drawing.Size(338, 20);
+            this.LastNameTxt.TabIndex = 13;
+            // 
+            // EmailTxt
+            // 
+            this.EmailTxt.Location = new System.Drawing.Point(112, 93);
+            this.EmailTxt.Margin = new System.Windows.Forms.Padding(1);
+            this.EmailTxt.Name = "EmailTxt";
+            this.EmailTxt.Size = new System.Drawing.Size(338, 20);
+            this.EmailTxt.TabIndex = 16;
+            // 
+            // PhoneNumberTxt
+            // 
+            this.PhoneNumberTxt.Location = new System.Drawing.Point(114, 212);
+            this.PhoneNumberTxt.Margin = new System.Windows.Forms.Padding(1);
+            this.PhoneNumberTxt.Name = "PhoneNumberTxt";
+            this.PhoneNumberTxt.Size = new System.Drawing.Size(338, 20);
+            this.PhoneNumberTxt.TabIndex = 21;
+            // 
+            // AddressTxt
+            // 
+            this.AddressTxt.Location = new System.Drawing.Point(114, 252);
+            this.AddressTxt.Margin = new System.Windows.Forms.Padding(1);
+            this.AddressTxt.Multiline = true;
+            this.AddressTxt.Name = "AddressTxt";
+            this.AddressTxt.Size = new System.Drawing.Size(338, 18);
+            this.AddressTxt.TabIndex = 23;
+            // 
+            // FirstDepositTxt
+            // 
+            this.FirstDepositTxt.Location = new System.Drawing.Point(114, 290);
+            this.FirstDepositTxt.Margin = new System.Windows.Forms.Padding(1);
+            this.FirstDepositTxt.Name = "FirstDepositTxt";
+            this.FirstDepositTxt.Size = new System.Drawing.Size(338, 20);
+            this.FirstDepositTxt.TabIndex = 24;
+            // 
+            // FirstNameTxt
+            // 
+            this.FirstNameTxt.Location = new System.Drawing.Point(112, 20);
+            this.FirstNameTxt.Margin = new System.Windows.Forms.Padding(1);
+            this.FirstNameTxt.Name = "FirstNameTxt";
+            this.FirstNameTxt.Size = new System.Drawing.Size(338, 20);
+            this.FirstNameTxt.TabIndex = 12;
+            // 
+            // RegisterBtn
+            // 
+            this.RegisterBtn.Location = new System.Drawing.Point(245, 325);
+            this.RegisterBtn.Margin = new System.Windows.Forms.Padding(1);
+            this.RegisterBtn.Name = "RegisterBtn";
+            this.RegisterBtn.Size = new System.Drawing.Size(76, 22);
+            this.RegisterBtn.TabIndex = 26;
+            this.RegisterBtn.Text = "Register";
+            this.RegisterBtn.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 297);
+            this.label6.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "First Deposit:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 255);
+            this.label5.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Home Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 215);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Phone Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 95);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "E-Mail:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Last Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "First Name:";
             // 
             // RegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 297);
+            this.ClientSize = new System.Drawing.Size(470, 361);
+            this.Controls.Add(this.PasswordTxt);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.DebitCardNumTxt);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.LastNameTxt);
@@ -213,20 +235,22 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button RegisterBtn;
-        private System.Windows.Forms.TextBox FirstNameTxt;
-        private System.Windows.Forms.TextBox FirstDepositTxt;
-        private System.Windows.Forms.TextBox AddressTxt;
-        private System.Windows.Forms.TextBox PhoneNumberTxt;
-        private System.Windows.Forms.TextBox EmailTxt;
-        private System.Windows.Forms.TextBox LastNameTxt;
+        private System.Windows.Forms.TextBox PasswordTxt;
+        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox DebitCardNumTxt;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox LastNameTxt;
+        private System.Windows.Forms.TextBox EmailTxt;
+        private System.Windows.Forms.TextBox PhoneNumberTxt;
+        private System.Windows.Forms.TextBox AddressTxt;
+        private System.Windows.Forms.TextBox FirstDepositTxt;
+        private System.Windows.Forms.TextBox FirstNameTxt;
+        private System.Windows.Forms.Button RegisterBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
