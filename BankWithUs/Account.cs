f1f6e2be911297e2ccadfe268dfabbd1d5a063cb
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-03-08 09:48:14 -0700

    Entered notes on various components

--- BankWithUs/Account.cs
+++ BankWithUs/Account.cs
@@ -7,14 +7,24 @@ using System.Threading.Tasks;
 
 namespace BankWithUs
 {
+    /// <summary>
+    /// This is the bank account class per user, all fields required for creation
+    /// This uses Data Annotations taken from : https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations?view=netframework-4.8
+    /// </summary>
     class Account
     {
+        /// <summary>
+        /// Key element for account, CustomerId
+        /// </summary>
         [Key]
         public int CustomerId { get; set; }
 
         [Required]
         public int AccountNum { get; set; }
 
+        /// <summary>
+        /// Required debitNum, validated same as a Credit Card
+        /// </summary>
         [Required]
         [CreditCard]
         public int DebitNum { get; set; }
@@ -27,14 +37,23 @@ namespace BankWithUs
         [StringLength(50)]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Required Email Address, simple validation as email address annotation
+        /// </summary>
         [Required]
         [EmailAddress]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Account pin range may be adjusted but should be a four digit pin, min 4, max 4 digits
+        /// </summary>
         [Required]
         [Range(4, 4)]
         public int AccountPin { get; set; }
 
+        /// <summary>
+        /// Phone number set as Phone validation
+        /// </summary>
         [Required]
         [Phone]
         public int PhoneNum { get; set; }
