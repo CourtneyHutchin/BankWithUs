1d6036b843f82a99baad292d8d05a00b95fe2f25
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-03-08 09:22:44 -0700

    Established primary properties

--- BankWithUs/Account.cs
+++ BankWithUs/Account.cs
@@ -8,5 +8,28 @@ namespace BankWithUs
 {
     class Account
     {
+
+        public int CustomerId { get; set; }
+
+        public int AccountNum { get; set; }
+
+        public int DebitNum { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string email { get; set; }
+
+        public int AccountPin { get; set; }
+
+        public int PhoneNum { get; set; }
+
+        public string Address { get; set; }
+
+        public int CheckingAmount { get; set; }
+
+        public int SavingsAmount { get; set; }
+
     }
 }
