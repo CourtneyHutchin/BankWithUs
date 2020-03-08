f3b78f081e4830a151758aab2beefef3feee0355
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-03-08 09:44:50 -0700

    Assigned Data Annotations And Ranges

--- BankWithUs/Account.cs
+++ BankWithUs/Account.cs
@@ -16,31 +16,37 @@ namespace BankWithUs
         public int AccountNum { get; set; }
 
         [Required]
+        [CreditCard]
         public int DebitNum { get; set; }
         
         [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
 
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
+        [Range(4, 4)]
         public int AccountPin { get; set; }
 
         [Required]
+        [Phone]
         public int PhoneNum { get; set; }
 
         [Required]
         public string Address { get; set; }
 
         [Required]
-        public int CheckingAmount { get; set; }
+        public double CheckingAmount { get; set; }
 
         [Required]
-        public int SavingsAmount { get; set; }
+        public double SavingsAmount { get; set; }
 
     }
 }
