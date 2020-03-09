5dd8a0936ed04811cf022aecf6ab197ce1945a34
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-03-09 14:42:56 -0700

    Added Migration

---
+++ BankWithUs/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace BankWithUs.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BankWithUs.BankContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BankWithUs.BankContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
